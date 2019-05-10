c7375857b2559f1378d6f99ad3eab882dc6d1e3a
DevWasi <wasibiit@gmail.com>
2019-05-10 07:14:57 +0500

    FilterByName

--- src/App.js
+++ src/App.js
@@ -1,39 +1,66 @@
 import React, { Component } from "react";
 import { records } from "./Users.json";
 import Record from "./components/Record";
+import logo from "./logo.svg";
+import Navbar from "react-bootstrap/Navbar";
+import CardColumns from "react-bootstrap/CardColumns";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      search: null,
-      records: []
+      searchByName: ""
     };
   }
 
-  updateSearch(event) {
-    this.setState({ search: event.target.value.substr(0, 20) });
+  updatesearchByName(event) {
+    this.setState({ searchByName: event.target.value.substr(0, 20) });
   }
+
   render() {
     console.log(records);
-    let filteredRecords = this.props.records.filter(record => {
+    let filteredRecords = records.filter(record => {
       return (
-        record.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !==
-        -1
+        record.userName
+          .toLowerCase()
+          .indexOf(this.state.searchByName.toLowerCase()) !== -1
       );
     });
     return (
       <div>
-        <input
-          type="text"
-          value={this.state.search}
-          onChange={this.updateSearch.bind(this)}
-        />
-        <ul>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              className="App-logo"
+              src={logo}
+              width="60"
+              height="60"
+            />
+            {" React-Filters"}
+          </Navbar.Brand>
+        </Navbar>
+        <InputGroup className="input">
+          <InputGroup.Prepend>
+            <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+          </InputGroup.Prepend>
+          <FormControl
+            placeholder="Username"
+            onChange={this.updatesearchByName.bind(this)}
+            value={this.state.searchByName}
+            aria-label="Username"
+            aria-describedby="basic-addon1"
+          />
+        </InputGroup>
+        <br />
+        <br />
+        <CardColumns>
           {filteredRecords.map(record => {
             return <Record record={record} key={record.id} />;
           })}
-        </ul>
+        </CardColumns>
       </div>
     );
   }
--- src/components/Record.jsx
+++ src/components/Record.jsx
@@ -1,8 +1,90 @@
 import React from "react";
+import "../App.css";
+import Card from "react-bootstrap/Card";
 
 function Record(props) {
-  //   console.log("Start", props);
-  return <div />;
+  // console.log(props);
+  return (
+    <Card id="team" class="pb-5">
+      <div class="image-flip" ontouchstart="this.classList.toggle('hover');">
+        <div class="mainflip">
+          <div class="frontside">
+            <div class="card">
+              <div class="card-body text-center">
+                <h4 class="card-title">{props.record.userName}</h4>
+                <h6>{props.record.email}</h6>
+                <h6>
+                  {props.record.country} | {props.record.ethnicity}
+                </h6>
+                <h6>
+                  {props.record.gender} | {props.record.age}
+                </h6>
+                <p class="card-text">
+                  This is basic card with image on top, title, description and
+                  button.
+                </p>
+                <a href="#" class="btn btn-primary btn-sm">
+                  <i class="fa fa-plus" />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="backside">
+            <div class="card">
+              <div class="card-body text-center mt-4">
+                <h6 class="backdis">Name: {props.record.userName}</h6>
+                <h6 class="backdis">Email: {props.record.email}</h6>
+                <h6 class="backdis">Contact: {props.record.phone}</h6>
+                <h6 class="backdis">Address: {props.record.address}</h6>
+                <h6 class="backdis">Age: {props.record.age}</h6>
+                <h6 class="backdis">Country: {props.record.country}</h6>
+                <h6 class="backdis">Ethnicity: {props.record.ethnicity}</h6>
+                <h6 class="backdis">Gender: {props.record.gender}</h6>
+                <ul class="list-inline">
+                  <li class="list-inline-item">
+                    <a
+                      class="social-icon text-xs-center"
+                      target="_blank"
+                      href="#"
+                    >
+                      <i class="fa fa-facebook" />
+                    </a>
+                  </li>
+                  <li class="list-inline-item">
+                    <a
+                      class="social-icon text-xs-center"
+                      target="_blank"
+                      href="#"
+                    >
+                      <i class="fa fa-twitter" />
+                    </a>
+                  </li>
+                  <li class="list-inline-item">
+                    <a
+                      class="social-icon text-xs-center"
+                      target="_blank"
+                      href="#"
+                    >
+                      <i class="fa fa-skype" />
+                    </a>
+                  </li>
+                  <li class="list-inline-item">
+                    <a
+                      class="social-icon text-xs-center"
+                      target="_blank"
+                      href="#"
+                    >
+                      <i class="fa fa-google" />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Card>
+  );
 }
 
 export default Record;
